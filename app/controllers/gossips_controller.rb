ac00d840757472c34469f3f0f57b4f9516e3c3e2
Dev-Moulin <p.moulin.95@gmail.com>
2024-10-31 12:20:18 +0100

    mise a jour du 31_10_2024 12h

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -18,13 +18,37 @@ class GossipsController < ApplicationController
       render new_gossip_path
     end
   end
-  def update
-  end
+  
 
+  
   def edit
     @gossip = Gossip.find(params[:id])
   end
+  
+  def update
+    @gossip = Gossip.find(params[:id])
+    if @gossip.update(gossip_params)
+      redirect_to @gossip
+    else
+      render :edit
+    end
+  end
+
   def show
     @gossip = Gossip.find(params[:id])
   end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossip_path
+  end
+  
+  private
+
+  def gossip_params
+    params.require(:gossip).permit(:title, :content)
+  end
 end
+
+
